2a3979a7efd0cd4ee5761a88ae958c549d250c83
dalifyyy <d4lified@gmail.com>
2026-05-23 14:50:03 +0800

    .

--- scripts/autoloads/registry.gd
+++ scripts/autoloads/registry.gd
@@ -5,5 +5,4 @@ var UID: Dictionary = {
 	"splash_screen": "uid://b4ggbq71lpwo4",
 	"title_screen": "uid://dix67ojrdr6ht",
 	"pause_screen": "uid://7jemke30bxqq",
-	"dialogue_box": "uid://6cae11kpahyv"
 }
