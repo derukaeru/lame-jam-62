e9e9acd6daa7d6f7f37eceb152bd3102449faaf8
dalifyyy <d4lified@gmail.com>
2026-05-23 15:51:14 +0800

    .

--- scenes/objects/paper.gd
+++ scenes/objects/paper.gd
@@ -6,7 +6,7 @@ var drag_offset := Vector2()
 var min_pos := Vector2(32, 32)
 var max_pos := Vector2(1028 - 32, 720 - 32)
 
-func _on_gui_input(event):
+func _on_gui_input(event) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			dragging = event.pressed
@@ -20,7 +20,7 @@ func _on_gui_input(event):
 		new_position.y = clamp(new_position.y, min_pos.y, max_pos.y)
 		global_position = new_position
 
-func _unhandled_input(event):
+func _unhandled_input(event) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 		dragging = false
 		z_index = 0
