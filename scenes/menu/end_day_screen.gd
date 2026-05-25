4c7de0e9b57ffbece6b70208261057e2c2adcf4d
dalifyyy <d4lified@gmail.com>
2026-05-25 11:39:03 +0800

    .

--- scenes/menu/end_day_screen.gd
+++ scenes/menu/end_day_screen.gd
@@ -1,30 +1,41 @@
 extends Control
 
-var leaving: bool = false
+var leaving: bool = true
 
 func _ready():
 	await get_tree().create_timer(1.0).timeout
 	$paper_processed.show()
-	await get_tree().create_timer(0.4).timeout
+	
+	await get_tree().create_timer(1.0).timeout
 	$paper_processed.text = "Reviewed Paper: " + str(GameManager.paper_today)
+	
 	await get_tree().create_timer(1.0).timeout
 	$paper_correct.show()
-	await get_tree().create_timer(0.4).timeout
+	
+	await get_tree().create_timer(1.0).timeout
 	$paper_correct.text = "Correctly Reviewed: " + str(GameManager.correct_paper)
+	
 	await get_tree().create_timer(1.0).timeout
 	$paper_wrong.show()
-	await get_tree().create_timer(0.4).timeout
+	
+	await get_tree().create_timer(1.0).timeout
 	$paper_wrong.text = "Wrongly Reviewed: " + str(GameManager.wrong_paper)
+	
 	await get_tree().create_timer(1.0).timeout
 	$chances.show()
-	await get_tree().create_timer(0.4).timeout
+	
+	await get_tree().create_timer(1.0).timeout
 	$chances.text = "Chances: " + str(GameManager.chance)
 	
 	if GameManager.wrong_paper > GameManager.correct_paper:
 		GameManager.chance -= 1
 		
-	await get_tree().create_timer(0.4).timeout
+	await get_tree().create_timer(0.7).timeout
 	$chances.text = "Chances: " + str(GameManager.chance)
+	
+	await get_tree().create_timer(1.0).timeout
+	$continue.show()
+	leaving = false
 
 func _process(_delta) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and not leaving:
